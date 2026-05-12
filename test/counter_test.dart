2449489d4c8ff44ee5f7b2e6462d1b915803ff4c
shroida <shroida012@gmail.com>
2026-05-12 11:29:19 +0300

    modify values

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,17 +2,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_widget_test/counter.dart';
 
 void main(){
-  test('Counter increments', () {
+  group('Test Counter Increnment and Decrenment', (){
     final counter = Counter();
-    expect(counter.value, 0);
+    counter.value = 7;
+ test('Counter increments', () {
+    expect(counter.value, 7);
     counter.increment();
-    expect(counter.value, 1);
+    expect(counter.value, 8 );
   });
 
   test('Counter decrements', () {
-    final counter = Counter();
-    expect(counter.value, 0);
+    expect(counter.value, 8);
     counter.decrement();
-    expect(counter.value, -1);
+    expect(counter.value, 7);
   });
+  });
+
 }
