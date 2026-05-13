375f2f74d3b6de43dbb8429825feb763b7fdf023
shroida <shroida012@gmail.com>
2026-05-13 09:49:41 +0300

    unit test all cases

--- lib/features/util/time_helper.dart
+++ lib/features/util/time_helper.dart
@@ -2,7 +2,7 @@ class TimeHelper {
 
   static String getTimeOfTheDay({DateTime? dateTime}) {
 
-    final hour = dateTime?.hour ?? DateTime(2026, 1, 1, 19).hour;
+    final hour = dateTime?.hour ?? DateTime(2026, 1, 1, 8).hour;
 
     if (hour >= 5 && hour < 12) {
       return "Morning";
